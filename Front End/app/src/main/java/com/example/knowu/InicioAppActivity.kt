fe21a02d47df87dbc6169838c5cd005c5a7e1d72
dylancolonhesi <dylan.reboucas@bandtec.com.br>
2022-02-24 22:09:37 -0300

    add splash activity

---
+++ Front End/app/src/main/java/com/example/knowu/InicioAppActivity.kt
@@ -0,0 +1,11 @@
+package com.example.knowu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_app)
+    }
+}
