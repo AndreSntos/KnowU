170b7e0b109140d4a001c1aec2c9c410574ed987
dylancolonhesi <dylan.reboucas@bandtec.com.br>
2022-02-26 17:59:15 -0300

    add password success

---
+++ Front End/app/src/main/java/com/example/knowu/PasswordSucessActivity.kt
@@ -0,0 +1,12 @@
+package com.example.knowu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PasswordSucessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_senha_alterada_sucesso)
+
+    }
+}
