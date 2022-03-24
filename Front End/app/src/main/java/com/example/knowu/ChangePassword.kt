6e038df1d40585136a5d2784b0746bfe630d6a34
renpaulino <renato.paulino@bandtec.com.br>
2022-03-23 22:02:43 -0300

    Telas de Insira o Código e Troque sua Senha

--- Front End/app/src/main/java/com/example/knowu/ChangePassword.kt
+++ Front End/app/src/main/java/com/example/knowu/ChangePassword.kt
@@ -3,9 +3,9 @@ package com.example.knowu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ForgotPassword : AppCompatActivity() {
+class ChangePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        setContentView(R.layout.activity_change_password)
     }
 }
