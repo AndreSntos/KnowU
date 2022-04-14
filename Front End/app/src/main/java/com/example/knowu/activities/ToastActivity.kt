d898fbacb09b404c53359da32ac603cea006bd5d
dylancolonhesi <dylan.colonhesi@bandtec.com.br>
2022-04-14 10:13:44 -0300

    fix: toast copiado

---
+++ Front End/app/src/main/java/com/example/knowu/activities/ToastActivity.kt
@@ -0,0 +1,11 @@
+package com.example.knowu.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toast)
+    }
+}
